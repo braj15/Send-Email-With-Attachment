3828272ba2f8e5e3930ac8a016b44b9575d3ea64
Biraj Raj <braj1602@gmail.com>
2018-03-05 00:41:09 +0530

    level_two

--- Aemail.py
+++ Aemail.py
@@ -2,15 +2,28 @@
 
 
 import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
 
 email_user = 'braj1902@gmail.com'
+email_password = 'thed@mnmailer123'
 email_rcver = 'braj95644@gmail.com'
 
-message = "Hi there, I'm sending this email using python! Pretty coool, huh!!!"
+subject = 'Python is cool!'
+
+msg = MIMEMultipart()
+msg['From'] = email_user
+msg['To'] = email_rcver
+msg['Subject'] = subject
+
+body = "Hi there, I'm sending this email using python! Pretty coool, huh!!!"
+msg.attach(MIMEText(body, 'plain'))
+text = msg.as_string()
 
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
-server.login(email_user, 'thed@mnmailer123')
+server.login(email_user, email_password)
 
-server.sendmail(email_user, email_rcver, message)
+server.sendmail(email_user, email_rcver, text)
 server.quit()
