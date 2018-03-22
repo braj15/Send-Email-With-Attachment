1dd28c05b77b573f2ba0ad799daa9c9677ca94b9
Biraj Raj <braj1602@gmail.com>
2018-03-22 22:14:04 +0530

    updated

--- Aemail.py
+++ Aemail.py
@@ -5,34 +5,41 @@ from email.mime.base import MIMEBase
 from email import encoders
 
 
-email_user = input("what is your gmail address? \n ")
-email_password = input("what is the password for that email address? \n  ")
-email_rcver = input("what email address do you want to send to? \n ")
+def main():
+    email_user = input("what is your gmail address? \n ")
+    email_password = input("what is the password for that email address? \n  ")
+    email_rcver = input("what email address do you want to send to? \n ")
 
-subject = input("Subject of the email : \n ")
+    subject = input("Subject of the email : \n ")
 
-msg = MIMEMultipart()
-msg['From'] = email_user
-msg['To'] = email_rcver
-msg['Subject'] = subject
+    msg = MIMEMultipart()
+    msg['From'] = email_user
+    msg['To'] = email_rcver
+    msg['Subject'] = subject
 
-body = input("write down body of the email : \n ")
-msg.attach(MIMEText(body, 'plain'))
+    body = input("write down body of the email : \n ")
+    msg.attach(MIMEText(body, 'plain'))
 
-filename = input("Enter filepath of the file you want to upload : \n ")
-attachment = open(filename, 'rb')
+    filename = input("Enter filepath of the file you want to upload : \n ")
+    attachment = open(filename, 'rb')
 
-part = MIMEBase('application', 'octet-stream')
-part.set_payload(attachment.read())
-encoders.encode_base64(part)
-part.add_header('content-disposition', "attachment; filename= "+filename)
+    part = MIMEBase('application', 'octet-stream')
+    part.set_payload(attachment.read())
+    encoders.encode_base64(part)
+    part.add_header('content-disposition', "attachment; filename= "+filename)
 
-msg.attach(part)
-text = msg.as_string()
+    msg.attach(part)
+    text = msg.as_string()
 
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
-server.login(email_user, email_password)
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(email_user, email_password)
 
-server.sendmail(email_user, email_rcver, text)
-server.quit()
+    server.sendmail(email_user, email_rcver, text)
+    server.quit()
+
+    print("Sent!")
+
+
+if __name__ == "__main__":
+    main()
